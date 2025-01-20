95a95ca72d3447262ab80f05299fc1a041ae0048
Vlad <vasilegin@gmail.com>
2025-01-21 00:47:14 +0300

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,83 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  alias: {
+    '@source': 'docs/.vuepress'
+  },
+  title: 'Архитектура высоконагруженных систем',
+  base: "/chappel/",
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: 'https://cdn-icons-png.flaticon.com/512/2083/2083213.png' }]
+  ],
+
+  theme: defaultTheme({
+      variables: {
+        nTasks: '5 + 1',
+        tasks: [
+            {
+                minScore: 5,
+                maxScore: 10
+            },
+            {
+                minScore: 5,
+                maxScore: 10
+            },
+            {
+                minScore: 10,
+                maxScore: 20
+            },
+            {
+                minScore: 5,
+                maxScore: 20
+            },
+            {
+                minScore: 5,
+                maxScore: 10
+            },
+            {
+                minScore: 5,
+                maxScore: 10
+            }
+        ]
+    },
+    repo: 'https://github.com/MANASLU8/chappel-source',
+    editLinks: true,
+    lastUpdated: true,
+    docsDir: 'docs',
+
+    navbar: [
+      {
+        text: 'Задания',
+        link: '/task/',
+      }
+    ],
+
+    sidebar: {
+      '/': [
+        {
+          title: 'Лабораторные работы',
+          collapsable: false,
+          children: [
+            'task/1',
+            'task/2',
+            'task/3',
+            'task/4',
+            'task/5',
+            'task/6'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      '@vuepress/plugin-search',
+    ]    
+  }),
+  bundler: viteBundler(),
+})
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroText: Архитектура высоконагруженных систем
+heroImage:  /logo.jpg
+tagline: Лабораторные работы
+actionLink: /task/
+# footer: Made by MANASLU8 with ❤️
+copyright: Customized copyright text
+---
